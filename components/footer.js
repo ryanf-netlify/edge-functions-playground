85be1ce7c9d74521541a2827480a487bb1da1a04
ryanf-netlify <ryan.flamion@netlify.com>
2022-08-30 10:56:42 -0500

    testing sync

--- components/footer.js
+++ components/footer.js
@@ -3,6 +3,7 @@ export default function footer() {
   const year = date.getFullYear();
   return `
     <footer class="footer">
+      <p >HELLO RY-GUY!</p>
       <p class="footer__copyright">© ${year} <a href="https://netlify.com/?utm_campaign=devex&utm_source=edge-functions-examples&utm_medium=web&utm_content=Netlify%20Home" target="_blank" rel="noopener">Netlify</a></p>
       <p class="footer__source"><a href="https://github.com/netlify/edge-functions-examples/" target="_blank" rel="noopener">View source code on GitHub</a></p>
     </footer>
